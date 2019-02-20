30f80fedbc1d62385a785a38cd97593da429a60d
Bernat Borrás Paronella <bernat.borras@privalia.com>
2019-02-20 15:31:53 +0100

    Fix koin crash

--- app/src/main/java/com/alorma/firebaseauth/demo/ui/UiModule.kt
+++ app/src/main/java/com/alorma/firebaseauth/demo/ui/UiModule.kt
@@ -5,7 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val uiModule = module {
+val uiModule = module(override = true) {
 
     viewModel { MainViewModel(get(), get()) }
 
